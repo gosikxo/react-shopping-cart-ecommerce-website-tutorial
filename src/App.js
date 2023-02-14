4cd770dc9b20d5aeb529af14eb96ef3a68f74330
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-02-14 18:10:37 +0100

    Add basic structure, router, navbar and style navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components/navbar';
 
-function App() {
+function App () {
   return (
     <div className="App">
-     
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
     </div>
   );
 }
