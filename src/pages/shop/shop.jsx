b8c4c73e7d2b553268e1e6c5778eb70bb03f7fe7
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-03-14 15:50:41 +0100

    Map through products and add product component

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { PRODUCTS } from '../../products'
+import { Product } from './product'
 
 const Shop = () => {
     return (
@@ -7,7 +9,9 @@ const Shop = () => {
                 <h1> The Shop </h1>
             </div>
             <div className='products'>
-                
+                {PRODUCTS.map((product) => (
+                    <Product data={product}/>
+                ))}
             </div>
         </div>
     )
