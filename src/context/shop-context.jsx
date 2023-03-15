6024acf4060cbde27ab57ee89818e7f71f554675
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-03-15 16:32:24 +0100

    Give other components access to context

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -5,14 +5,14 @@ export const ShopContext = createContext(null)
 
 const getDefaultCart = () => {
     let cart = {}
-    for (let i = 0; i < PRODUCTS.length + 1; i++) {
+    for (let i = 1; i < PRODUCTS.length + 1; i++) {
         cart[i] = 0
     }
     return cart
 }
 
 export const ShopContextProvider = (props) => {
-    const [cartItem, setCartItems] = useState(getDefaultCart())
+    const [cartItems, setCartItems] = useState(getDefaultCart())
 
     const addToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
@@ -22,8 +22,10 @@ export const ShopContextProvider = (props) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
+    const contextValue = {cartItems, addToCart, removeFromCart}
+    console.log(cartItems)
     return (
-        <ShopContext.Provider>
+        <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
     )
