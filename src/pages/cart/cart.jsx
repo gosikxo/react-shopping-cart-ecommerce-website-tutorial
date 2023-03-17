5847b2df56bd11ddbf5985242bbdd3e7dddc6e32
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-03-17 12:58:40 +0100

    Add products to the cart and style the cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { PRODUCTS } from '../../products'
+import { ShopContext } from '../../context/shop-context'
+import { CartItem } from './cart-item'
+import { useContext } from 'react'
+import "./cart.css"
 
 export const Cart = () => {
+    const { cartItems } = useContext(ShopContext)
+
     return (
-        <div>cart</div>
+        <div className='cart'>
+            <div>
+                <h1>Your Cart Items</h1>
+            </div>
+            <div className='cartItems'>
+                {PRODUCTS.map((product) => {
+                    if (cartItems[product.id] !== 0) {
+                        return <CartItem data={product} />
+                    }
+                })}
+            </div>
+        </div>
     )
 }
